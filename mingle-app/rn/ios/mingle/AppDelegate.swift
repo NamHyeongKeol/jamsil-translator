317ae05aa44274e6ced6d1a10617b0e81c86d125
NamHyeongKeol <grey_pastor@naver.com>
2026-02-27 21:44:14 +0900

    Rename rnnative to mingle across RN iOS/Android

--- mingle-app/rn/ios/mingle/AppDelegate.swift
+++ mingle-app/rn/ios/mingle/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "rnnative",
+      withModuleName: "mingle",
       in: window,
       launchOptions: launchOptions
     )
